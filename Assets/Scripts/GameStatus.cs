08513e5f3e4d921c4229195bb7219ca095ff58d8
pandu1990 <pandu1990@gmail.com>
2018-09-20 23:16:31 -0700

    'Prototype'

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStatus : MonoBehaviour {
+
+    [SerializeField] int totalPoints = 0;
+    [SerializeField] Text scoreText;
+    [SerializeField] Text WinLoseText;
+
+    public void Start()
+    {
+        WinLoseText.text = "";
+        DisplayScore();
+    }
+
+    public void addToScore(int points)
+    {
+        totalPoints += points;
+        if (totalPoints < 0)
+        {
+            WinLoseText.text = "You Lose!";
+            WinLoseText.color = Color.red;
+        } else if (totalPoints > 100)
+        {
+            WinLoseText.text = "You Win!";
+        }
+        DisplayScore();
+    }
+
+    private void DisplayScore()
+    {
+        scoreText.text = totalPoints.ToString();
+    }
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Pickup : MonoBehaviour {
+
+    [SerializeField] TextMeshProUGUI tmproText;
+    [SerializeField] int points = 0;
+
+    GameStatus gameStatus;
+
+	// Use this for initialization
+	void Start () {
+        gameStatus = FindObjectOfType<GameStatus>();
+        tmproText.text = points.ToString();
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameStatus.addToScore(points);
+        Destroy(gameObject);
+    }
+
+}
