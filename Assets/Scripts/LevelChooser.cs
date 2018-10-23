23dff5d6da4e9d19890eacf4112a62ba55ce2ec3
souravpati <sourav.vky@gmail.com>
2018-10-22 17:16:01 -0700

    Intro Scene

---
+++ Assets/Scripts/LevelChooser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelChooser : MonoBehaviour {
+
+
+    private Button thisButton;
+    private ConvertSceneScript convertScene;
+    [SerializeField] int level;
+	// Use this for initialization
+    void Start () {
+        convertScene = FindObjectOfType<ConvertSceneScript>();
+        thisButton = GetComponent<Button>();
+        thisButton.GetComponentInChildren<Text>().text = level.ToString();
+        thisButton.onClick.AddListener(delegate {
+            SwitchLevel(level);
+        });
+	}
+
+    void SwitchLevel(int level) {
+        Debug.Log(level);
+        convertScene.loadLevel(level);
+    }
+}
